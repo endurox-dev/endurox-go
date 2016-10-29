43620df3bb67ea5d4f3396ce3959227d458f2c91
spuhpointer <madars.vitolins@gmail.com>
2016-10-29 16:02:59 +0300

    Migrating to object api

--- tests/03_basic_carray_call/src/cclient/client.go
+++ tests/03_basic_carray_call/src/cclient/client.go
@@ -3,42 +3,48 @@ package main
 import (
 	"atmi"
 	"fmt"
-        "os"
+	"os"
 )
 
 const (
-        SUCCEED = 0
-        FAIL = -1
+	SUCCEED = 0
+	FAIL    = -1
 )
 
 //Binary main entry
 func main() {
 
-        ret:=SUCCEED
+	ret := SUCCEED
+	var ac *atmi.ATMICtx
+	var err atmi.ATMIError
+	//Return to the caller (kind of destructor..)
+	defer func() {
+		if nil != ac {
+			ac.TpTerm()
+			ac.FreeATMICtx() // Kill the context
+		}
+		os.Exit(ret)
+	}()
 
-        bytes := []byte{9,8,7,6,5,4,3,2,1,0}
-        buf, err := atmi.NewCarray(bytes)
+	bytes := []byte{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	buf, err := ac.NewCarray(bytes)
 
-        if err != nil {
-                fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message());
-                ret = FAIL
-                goto out
-        }
+	if err != nil {
+		fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
+		ret = FAIL
+		return
+	}
 
-        fmt.Printf("Sending: [%v]\n", buf.GetBytes())
+	fmt.Printf("Sending: [%v]\n", buf.GetBytes())
 
-        //Call the server
-        if _, err:=atmi.TpCall("TESTSVC", buf, 0); nil!=err {
-                fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message());
-                ret = FAIL
-                goto out
-        }
-        
-        //Print the output buffer
-        fmt.Printf("Got response: [%v]\n", buf.GetBytes())
-        
-out:
-        //Close the ATMI session
-        atmi.TpTerm()
-        os.Exit(ret)
+	//Call the server
+	if _, err := ac.TpCall("TESTSVC", buf, 0); nil != err {
+		fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
+		ret = FAIL
+		return
+	}
+
+	//Print the output buffer
+	fmt.Printf("Got response: [%v]\n", buf.GetBytes())
+
 }
--- tests/03_basic_carray_call/src/cserver/server.go
+++ tests/03_basic_carray_call/src/cserver/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"atmi"
 	"fmt"
+	"os"
 )
 
 const (
@@ -11,42 +12,42 @@ const (
 )
 
 //TESTSVC service
-func TESTSVC(svc *atmi.TPSVCINFO) {
+func TESTSVC(ac *atmi.ATMICtx, svc *atmi.TPSVCINFO) {
 
 	ret := SUCCEED
-        somebytes := []byte{0,1,2,3,4,5,6,7,8,9,10}
+	somebytes := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	//Get Typed String Handler
-	c, _ := atmi.CastToCarray(&svc.Data)
+	c, _ := ac.CastToCarray(&svc.Data)
 
 	//Print the buffer to stdout
-        fmt.Printf("Incoming request: [%v]\n", c.GetBytes())
+	fmt.Printf("Incoming request: [%v]\n", c.GetBytes())
 
-        //Resize buffer, to have some more space
-        if err :=c.TpRealloc(128); err!=nil {
+	//Resize buffer, to have some more space
+	if err := c.TpRealloc(128); err != nil {
 		fmt.Printf("Got error: %d:[%s]\n", err.Code(), err.Message())
-                ret = FAIL
-                goto out
-        }
+		ret = FAIL
+		goto out
+	}
 
 	//Send some bytes back...
-        c.SetBytes(somebytes);
+	c.SetBytes(somebytes)
 
 out:
-        //Return to the caller
-        if SUCCEED==ret {
-        	atmi.TpReturn(atmi.TPSUCCESS, 0, &c, 0)
-        } else {
-        	atmi.TpReturn(atmi.TPFAIL, 0, &c, 0)
-        }
+	//Return to the caller
+	if SUCCEED == ret {
+		ac.TpReturn(atmi.TPSUCCESS, 0, &c, 0)
+	} else {
+		ac.TpReturn(atmi.TPFAIL, 0, &c, 0)
+	}
 	return
 }
 
 //Server init
-func Init() int {
+func Init(ac *atmi.ATMICtx) int {
 
 	//Advertize TESTSVC
-	if err := atmi.TpAdvertise("TESTSVC", "TESTSVC", TESTSVC); err != nil {
+	if err := ac.TpAdvertise("TESTSVC", "TESTSVC", TESTSVC); err != nil {
 		fmt.Println(err)
 		return atmi.FAIL
 	}
@@ -55,14 +56,21 @@ func Init() int {
 }
 
 //Server shutdown
-func Uninit() {
+func Uninit(ac *atmi.ATMICtx) {
 	fmt.Println("Server shutting down...")
 }
 
 //Executable main entry point
 func main() {
+	//Have some context
+	ac, err := atmi.NewATMICtx()
 
-	//Run as server
-	atmi.TpRun(Init, Uninit)
+	if nil != err {
+		fmt.Errorf("Failed to allocate cotnext!", err)
+		os.Exit(atmi.FAIL)
+	} else {
+		//Run as server
+		ac.TpRun(Init, Uninit)
+		ac.FreeATMICtx()
+	}
 }
-
