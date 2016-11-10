a15f6e3845df3331c6b197437c2d18392ee08c11
spuhpointer <madars.vitolins@gmail.com>
2016-11-10 21:13:12 +0200

    Moving to object-api

--- tests/07_basic_durable_queue/src/sclient/client.go
+++ tests/07_basic_durable_queue/src/sclient/client.go
@@ -4,6 +4,7 @@ import (
 	"atmi"
 	"fmt"
 	"os"
+	"runtime"
 )
 
 const (
@@ -17,54 +18,70 @@ const (
 func main() {
 
 	ret := SUCCEED
-	defer func() { atmi.TpTerm(); os.Exit(ret) }()
+	defer func() { os.Exit(ret) }()
 
-	var qctl atmi.TPQCTL
+	//Have some loop for memory leak checks...
+	for i := 0; i < 1000; i++ {
 
-	testMessage := "Hello World from queue"
+		var ac *atmi.ATMICtx
+		var err atmi.ATMIError
+		//Allocate context
+		ac, err = atmi.NewATMICtx()
+		if nil != err {
+			fmt.Errorf("Failed to allocate cotnext!", err)
+			ret = FAIL
+			return
+		}
 
-	buf, err := atmi.NewString(testMessage)
+		var qctl atmi.TPQCTL
 
-	if err != nil {
-		fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
-		ret = FAIL
-		return
+		testMessage := "Hello World from queue"
+
+		buf, err := ac.NewString(testMessage)
+
+		if err != nil {
+			fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
+			ret = FAIL
+			return
+		}
+
+		//Enqueue the string
+		if err := ac.TpEnqueue("QSPACE1", "MYQ1", &qctl, buf, 0); nil != err {
+			fmt.Printf("TpEnqueue() failed: ATMI Error %d:[%s]\n", err.Code(), err.Message())
+			ret = FAIL
+			return
+		}
+
+		fmt.Printf("Enqueue OK\n")
+
+		//Allocate new return buffer, to ensure that this is different one..!
+		buf2, err := ac.NewString("")
+
+		if err != nil {
+			fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
+			ret = FAIL
+			return
+		}
+
+		if err := ac.TpDequeue("QSPACE1", "MYQ1", &qctl, buf2, 0); nil != err {
+			fmt.Printf("TpDequeue() failed: ATMI Error %d:[%s]\n", err.Code(), err.Message())
+			ret = FAIL
+			return
+		}
+
+		//Print the output buffer
+		fmt.Printf("Dequeued message: [%s]\n", buf2.GetString())
+
+		if buf2.GetString() != testMessage {
+			fmt.Printf("ERROR ! Enqueued [%s] but dequeued [%s]",
+				testMessage, buf2.GetString())
+			ret = FAIL
+			return
+		}
+		fmt.Printf("Message machged ok!\n")
+
+		runtime.GC()
 	}
-
-	//Enqueue the string
-	if err := atmi.TpEnqueue("QSPACE1", "MYQ1", &qctl, buf, 0); nil != err {
-		fmt.Printf("TpEnqueue() failed: ATMI Error %d:[%s]\n", err.Code(), err.Message())
-		ret = FAIL
-		return
-	}
-
-	fmt.Printf("Enqueue OK\n")
-
-	//Allocate new return buffer, to ensure that this is different one..!
-	buf2, err := atmi.NewString("")
-
-	if err != nil {
-		fmt.Printf("ATMI Error %d:[%s]\n", err.Code(), err.Message())
-		ret = FAIL
-		return
-	}
-
-	if err := atmi.TpDequeue("QSPACE1", "MYQ1", &qctl, buf2, 0); nil != err {
-		fmt.Printf("TpDequeue() failed: ATMI Error %d:[%s]\n", err.Code(), err.Message())
-		ret = FAIL
-		return
-	}
-
-	//Print the output buffer
-	fmt.Printf("Dequeued message: [%s]\n", buf2.GetString())
-
-	if buf2.GetString() != testMessage {
-		fmt.Printf("ERROR ! Enqueued [%s] but dequeued [%s]",
-			testMessage, buf2.GetString())
-		ret = FAIL
-		return
-	}
-	fmt.Printf("Message machged ok!\n")
 
 	return
 }
