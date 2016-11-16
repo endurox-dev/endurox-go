fa7adf80f411c384bc7dde38d10d94f00154b6ca
spuhpointer <madars.vitolins@gmail.com>
2016-11-16 11:06:10 +0200

    Works on document updater

--- doc/docupdate.pl
+++ doc/docupdate.pl
@@ -21,9 +21,12 @@ foreach my $file (@ARGV)
 
 	my $lines = "";
 	my $was_comment = 0;
+	my $was_func_start = 0;
 	while (<$fh>) {
 		# line contents's automatically stored in the $_ variable
 		#print $_;
+		chomp;
+		
 		my $line = $_;
 		
 		#print $line ;
@@ -36,30 +39,44 @@ foreach my $file (@ARGV)
 		}
 		elsif($line =~/^func/)
 		{
+		
+			if ($line=~/^func.*[^\{]\s*$/)
+			{
+				# We need to read some more lines here and join them
+				# util we get the scope open symbol
+				while (<$fh>)
+				{
+					chomp;
+					if ($line =~/^func.*[^\{]\s*$/)
+					{
+						last;
+					}
+					$line = $line.$_;
+				}
+			}
 			# Ok This is our func, get the structure or it is global/atmi.
 			
-			
 			# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64) ATMIError {
-			print $line;
+			print "got [$line]\n";
 			if ($line =~/^func\s*\(.*\)\s*[A-Za-z0-9_]*\(.*\)\s*\(.*\)\s*{/)
 			{
 				# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64) (int, ATMIError) {
-				print "1: ".$line;
+				print "1: [$line]\n";
 			}
 			elsif ($line =~/^func\s*\(.*\)\s*[A-Za-z0-9_]*\(.*\)\s*[0-9A-Za-z_\*]*\s*{/)
 			{
 				# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64)  ATMIError {
-				print "2: ".$line;
+				print "2: [$line]\n";
 			}
-			if ($line =~/^func\s*[A-Za-z0-9_]*\(.*\)\s*\([0-9A-Za-z_]*\)\s*{/)
+			elsif ($line =~/^func\s*[A-Za-z0-9_]*\(.*\)\s*\([0-9A-Za-z_]*\)\s*{/)
 			{
 				# func NewATMICtx() (*ATMICtx, ATMIError) {
-				print "3: ".$line;
+				print "3: [$line]\n";
 			}
 			elsif ($line =~/^func\s*[A-Za-z0-9_]*\(.*\)\s*[A-Za-z0-9_\*]*s*{/)
 			{
 				# func NewATMICtx() ATMIError {
-				print "4: ".$line;
+				print "4: [$line]\n";
 			}
 		}
 		else
