5aa4a0785ec873cc121d8d83c7d9812f14cce777
spuhpointer <madars.vitolins@gmail.com>
2016-11-18 13:16:53 +0200

    Works on document generator

--- doc/docupdate.pl
+++ doc/docupdate.pl
@@ -15,7 +15,7 @@
 %M_func = ();
 
 
-foreach my $file (@ARGV)
+NEXT: foreach my $file (@ARGV)
 {
 	open my $fh, '<', $file or die $!;
 
@@ -35,11 +35,16 @@ foreach my $file (@ARGV)
 			# Strip off the comment
 			$line = substr $line, 2;
 			$was_comment = 1;
-			$lines = $lines.$line;
+			$lines = $lines.' '.$line;
 		}
 		elsif($line =~/^func/)
 		{
 		
+			my $func = "";
+			my $struct = "atmi";
+			my $return = "";
+			my $def = "";
+			
 			if ($line=~/^func.*[^\{]\s*$/)
 			{
 				# We need to read some more lines here and join them
@@ -57,16 +62,24 @@ foreach my $file (@ARGV)
 			# Ok This is our func, get the structure or it is global/atmi.
 			
 			# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64) ATMIError {
+			
 			print "got [$line]\n";
+			($def) = ($line =~ /(.*)\s\{/);
+			
+			print "func definition [$def]\n";
+			
+			
 			if ($line =~/^func\s*\(.*\)\s*[A-Za-z0-9_]*\(.*\)\s*\(.*\)\s*{/)
 			{
 				# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64) (int, ATMIError) {
 				print "1: [$line]\n";
+				($struct, $func, $return) = ($line =~/^func\s*\(.*\s\**(.*)\)\s*([A-Za-z0-9_]*)\(.*\)\s*\((.*)\)\s*{/);
 			}
 			elsif ($line =~/^func\s*\(.*\)\s*[A-Za-z0-9_]*\(.*\)\s*[0-9A-Za-z_\*]*\s*{/)
 			{
 				# func (ac *ATMICtx) TpACall(svc string, tb TypedBuffer, flags int64)  ATMIError {
 				print "2: [$line]\n";
+				($struct, $func, $return) = ($line =~/^func\s*\(.*\s\**(.*)\)\s*([A-Za-z0-9_]*)\(.*\)\s*([0-9A-Za-z_\*]*)\s*{/);
 			}
 			elsif ($line =~/^func\s*[A-Za-z0-9_]*\(.*\)\s*\([0-9A-Za-z_]*\)\s*{/)
 			{
@@ -78,14 +91,94 @@ foreach my $file (@ARGV)
 				# func NewATMICtx() ATMIError {
 				print "4: [$line]\n";
 			}
+			
+			print "func: [$func]\n";
+			print "struct: [$struct]\n";
+			print "return: [$return]\n";
+			
+			print "Building doc... (only global func, starting with capital letter)\n";
+			
+			if ($func!~/$[A-Z]/)
+			{
+				next NEXT;
+			}
+			
+			
+			my @fields = split /@/, $lines;
+			
+			my $descr = $fields[0];
+			my $retdescr = "";
+			my $varname = "";
+			my $vardescr = "";
+			
+			$descr = $descr."\n";
+			my $have_params = 0;
+			
+			for (my $i=1; $i < scalar @fields; $i++) {
+				
+				if ($fields[$i]=~/$param/)
+				{
+					($varname, $vardescr) = ($fields[$i] =~ /$param\s*([^\s]*)\s*(.*)/);
+					
+					if (!$have_params)
+					{
+						$descr = $descr. "*$varname* is $vardescr.";
+						$have_params = 1;
+					}
+					else
+					{
+						$descr = $descr. "\n*$varname* is $vardescr.";	
+					}
+				}
+				elsif ($fields[$i]=~/$return/)
+				{
+					($retdescr) = ($fields[$i] =~ /$return\s*(.*)\s*/);
+				}
+			}
+			
+			
+			my $final_block = "";
+			
+			if ($retdescr eq "")
+			{
+				$final_block = <<"END_MESSAGE";
+[cols="h,5a"]
+|===
+| Function
+| $def
+| Description
+| $descr
+|===
+END_MESSAGE
+			}
+			else
+			{
+				$final_block = <<"END_MESSAGE";
+[cols="h,5a"]
+|===
+| Function
+| $def
+| Description
+| $descr
+| Returns
+| $retdescr
+|===
+END_MESSAGE
+			}
+			
+			print "***************GENERATED DOC ****************\n";
+			print "$final_block\n";
+			print "*********************************************\n";
+			
+			# Link to the key
+			$M_func{"$struct\.$func"} = $final_block;
 		}
 		else
 		{
 			$was_comment = 0;
 			$lines = "";
 		}
-		
-		
+	
 	}
 	close $fh or die $!;
 
