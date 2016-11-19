b89c0db5da595abe43551e46985885cc61e373be
spuhpointer <madars.vitolins@gmail.com>
2016-11-19 12:32:23 +0200

    Works on doc

--- doc/docupdate.pl
+++ doc/docupdate.pl
@@ -1,28 +1,56 @@
 #!/usr/bin/perl
 
+
+use Text::Wrap;
+
 #
 # @(#) Read all files and group the functions by objects
 #
 
-# https://perlmaven.com/how-to-sort-a-hash-in-perl
-#foreach my $name (sort { $planets{$a} <=> $planets{$b} or $a cmp $b } keys %planets) {
-#    printf "%-8s %s\n", $name, $planets{$name};
-#}
+my $M_doc = 'endurox-go-book.adoc';
 
 #
 # So we will store Object.Func.
 #
-%M_func = ();
+my %M_func = ();
 
 
-NEXT: foreach my $file (@ARGV)
+#
+# Support funcs
+#
+
+sub read_file {
+    my ($filename) = @_;
+ 
+    open my $in, '<:encoding(UTF-8)', $filename or die "Could not open '$filename' for reading $!";
+    local $/ = undef;
+    my $all = <$in>;
+    close $in;
+ 
+    return $all;
+}
+ 
+sub write_file {
+    my ($filename, $content) = @_;
+ 
+    open my $out, '>:encoding(UTF-8)', $filename or die "Could not open '$filename' for writing $!";;
+    print $out $content;
+    close $out;
+ 
+    return;
+}
+
+
+$Text::Wrap::columns = 80;
+
+foreach my $file (@ARGV)
 {
 	open my $fh, '<', $file or die $!;
 
 	my $lines = "";
 	my $was_comment = 0;
 	my $was_func_start = 0;
-	while (<$fh>) {
+NEXT:	while (<$fh>) {
 		# line contents's automatically stored in the $_ variable
 		#print $_;
 		chomp;
@@ -96,13 +124,12 @@ NEXT: foreach my $file (@ARGV)
 			print "struct: [$struct]\n";
 			print "return: [$return]\n";
 			
-			print "Building doc... (only global func, starting with capital letter)\n";
-			
-			if ($func!~/$[A-Z]/)
+			if ($func!~/^[A-Z]/)
 			{
+				print "Func does not start with capital\n";
 				next NEXT;
 			}
-			
+			print "Building doc...\n";
 			
 			my @fields = split /@/, $lines;
 			
@@ -111,33 +138,42 @@ NEXT: foreach my $file (@ARGV)
 			my $varname = "";
 			my $vardescr = "";
 			
-			$descr = $descr."\n";
-			my $have_params = 0;
+			$descr =~ s/^\s+|\s+$//g;
+			
+			$descr = "$descr. ";
+			#my $have_params = 0;
 			
 			for (my $i=1; $i < scalar @fields; $i++) {
 				
-				if ($fields[$i]=~/$param/)
+				if ($fields[$i]=~/^param/)
 				{
-					($varname, $vardescr) = ($fields[$i] =~ /$param\s*([^\s]*)\s*(.*)/);
+					($varname, $vardescr) = ($fields[$i] =~ /^param\s*([^\s]*)\s*(.*)\s*/);
 					
-					if (!$have_params)
-					{
-						$descr = $descr. "*$varname* is $vardescr.";
-						$have_params = 1;
-					}
-					else
-					{
-						$descr = $descr. "\n*$varname* is $vardescr.";	
-					}
+					$varname =~ s/^\s+|\s+$//g;
+					$vardescr =~ s/^\s+|\s+$//g;
+					
+					$descr = $descr."\n*$varname* $vardescr. ";
 				}
-				elsif ($fields[$i]=~/$return/)
+				elsif ($fields[$i]=~/^return/)
 				{
-					($retdescr) = ($fields[$i] =~ /$return\s*(.*)\s*/);
+					($retdescr) = ($fields[$i] =~ /^return\s*(.*)\s*/);
+					
+					$retdescr =~ s/^\s+|\s+$//g;
 				}
 			}
 			
 			
 			my $final_block = "";
+			my $server_block = "";
+			
+			if ($file=~/atmisrv.go/)
+			{
+				$server_block="To XATMI server";
+			}
+			else
+			{
+				$server_block="XATMI client and server";
+			}
 			
 			if ($retdescr eq "")
 			{
@@ -148,7 +184,10 @@ NEXT: foreach my $file (@ARGV)
 | $def
 | Description
 | $descr
+| Applies
+| $server_block
 |===
+
 END_MESSAGE
 			}
 			else
@@ -162,9 +201,13 @@ END_MESSAGE
 | $descr
 | Returns
 | $retdescr
+| Applies
+| $server_block
 |===
+
 END_MESSAGE
 			}
+			$final_block = wrap('', '', $final_block);
 			
 			print "***************GENERATED DOC ****************\n";
 			print "$final_block\n";
@@ -183,5 +226,105 @@ END_MESSAGE
 	close $fh or die $!;
 
 }
+
+### OK Seems like we got the stuff out, now need to sort and plot the doc
+# https://perlmaven.com/how-to-sort-a-hash-in-perl
+
+
+my $topic = "";
+
+my $output = "";
+
+foreach my $name (sort { $M_func{$a} <=> $M_func{$b} or $a cmp $b } keys %M_func)
+{
+
+	printf "SORTED: %-8s %s\n", $name, $M_func{$name};
+
+	if ($name=~/^atmi\./ && $topic ne "atmi")
+	{
+		$topic = "atmi";
+		$output = $output."=== ATMI Package functions\n";
+	}
+	elsif ($name=~/^nstdError\./ && $topic ne "nstdError")
+	{
+		$topic = "nstdError";
+		$output = $output."=== Enduro/X Standard Error Object / NSTDError interface\n";
+	}
+	elsif ($name=~/^TypedJSON\./ && $topic ne "TypedJSON")
+	{
+		$topic = "TypedJSON";
+		$output = $output."=== JSON IPC buffer format\n";
+	}
+	elsif ($name=~/^TypedString\./ && $topic ne "TypedString")
+	{
+		$topic = "TypedString";
+		$output = $output."=== String IPC buffer format\n";
+	}
+	elsif ($name=~/^TypedUBF\./ && $topic ne "TypedUBF")
+	{
+		$topic = "TypedUBF";
+		$output = $output."=== UBF Key/value IPC buffer format\n";
+	}
+	elsif ($name=~/^TypedCarray\./ && $topic ne "TypedCarray")
+	{
+		$topic = "TypedCarray";
+		$output = $output."=== Binary buffer IPC buffer format\n";
+	}
+	elsif ($name=~/^ATMIBuf\./ && $topic ne "ATMIBuf")
+	{
+		$topic = "ATMIBuf";
+		$output = $output."=== Abstract IPC buffer - ATMIUbf\n";
+	}
+	elsif ($name=~/^ATMICtx\./ && $topic ne "ATMICtx")
+	{
+		$topic = "ATMICtx";
+		$output = $output."=== ATMI Context\n";
+		$output = $output."This concentrates most of the ATMI Enduro/X functionality. And is able to run multiple contexts in Go routines\n";
+	}
+	elsif ($name=~/^atmiError\./ && $topic ne "atmiError")
+	{
+		$topic = "atmiError";
+		$output = $output."=== ATMI Error object / ATMIError interface\n";
+	}
+	elsif ($name=~/^ubfError\./ && $topic ne "ubfError")
+	{
+		$topic = "ubfError";
+		$output = $output."=== BUF Error object/ UBFError interface\n";
+	}
+	
+	
+	# Have title for function
+	
+	#my ($funcname) = ($name =~ /^.*\.(.*)/);
+	
+	$output = $output."==== $name()\n";
+	
+	
+	$output = $output.$M_func{$name}."\n";
+    
+}
+
+print $output;
+
+
+#
+# Got to replace text between two anchors..
+#
+if (-e $M_doc)
+{
+	my $data = read_file($M_doc);
+	#$data =~ s/Copyright Start-Up/Copyright Large Corporation/g;
+	
+	$data =~ s/(\[\[gen_doc-start\]\]\n)(.+?)(\[\[gen_doc-stop\]\]\n)/$1$output$2/s;
+	
+	write_file($M_doc, $data);
+	exit;
+}
+else
+{
+	print STDERR "$M_doc does not exists in current directory!\n";
+	exit -1
+}
+
 
 
