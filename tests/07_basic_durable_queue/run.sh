14306922006acc493536ea7762dd7566a8733950
Test user <user1@freebsd11.silodev.eu>
2017-11-20 12:25:04 +0200

    Fixes on unit tests

--- tests/07_basic_durable_queue/run.sh
+++ tests/07_basic_durable_queue/run.sh
@@ -9,6 +9,10 @@ cd conf
 . setndrx
 popd
 
+# Seems on freebsd we have an issue with stack sizes, as cgo limit is 2M
+# Support #251
+export NDRX_MSGSIZEMAX=100000
+
 # Start the enduro/x app server (which will boot the our server executable)
 
 xadmin start -y
